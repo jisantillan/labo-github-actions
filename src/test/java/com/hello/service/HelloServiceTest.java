fa5e7730117a76ad69041aa6f70e1b63f2029244
jisantillan <javierignaciosantillan.u@gmail.com>
2025-05-02 00:13:59 -0300

    update fix  test

--- src/test/java/com/hello/service/HelloServiceTest.java
+++ src/test/java/com/hello/service/HelloServiceTest.java
@@ -17,6 +17,6 @@ public class HelloServiceTest {
     @Test
     public void testHelloMessage() {
         String result = helloService.getHello();
-        assertEquals("Bye, Spring Boot!", result);
+        assertEquals("Hello, Spring Boot!", result);
     }
 }
